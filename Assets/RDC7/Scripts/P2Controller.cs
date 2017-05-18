05c3c458992f54ac961e9810c6e8e4c634d07ec9
Joaquin Cardenas <jcardenas.lie@gmail.com>
2017-05-17 22:08:21 -0400

    creacion brillo en pantalla

--- Assets/RDC7/Scripts/P2Controller.cs
+++ Assets/RDC7/Scripts/P2Controller.cs
@@ -5,14 +5,37 @@ using UnityEngine.UI;
 
 public class P2Controller : MonoBehaviour {
 
-    Image []images;
-	// Use this for initialization
-	void Start () {
-		
+    public Image []imgBanca;
+    public Image []imgArbol;
+    public Image imgFuente;
+    public Image imgVereda;
+
+    public Image imgBrillo;
+
+    public int waitTime = 1;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("Example");	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private IEnumerator Example()
+    {
+        Debug.Log("Time " + Time.time);
+
+        Image tmp_brillo = (Image)
+                        Instantiate(imgBrillo);
+
+        tmp_brillo.transform.SetParent(imgBanca[0].transform);
+        //Instantiate(imgBrillo, 
+        //    new Vector2(imgBanca[0].rectTransform.position.x, 
+        //        imgBanca[0].rectTransform.position.y), 
+        //    Quaternion.identity);
+        yield return new WaitForSeconds(waitTime);
+        
+    }
 }
