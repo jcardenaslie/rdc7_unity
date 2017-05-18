abd037daabbee3f0b349e0418158a0673e148401
Joaquin Cardenas <jcardenas.lie@gmail.com>
2017-05-18 18:47:47 -0400

    algo extraño paso

--- Assets/RDC7/Scripts/P2Controller.cs
+++ Assets/RDC7/Scripts/P2Controller.cs
@@ -9,33 +9,58 @@ public class P2Controller : MonoBehaviour {
     public Image []imgArbol;
     public Image imgFuente;
     public Image imgVereda;
-
     public Image imgBrillo;
+
+    private GameObject btnNext; 
 
     public int waitTime = 1;
     // Use this for initialization
     void Start () {
-        StartCoroutine("Example");	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        StartCoroutine("RutinaBrillos");
+        DeactivateNextButton();
 	}
 
-    private IEnumerator Example()
+    private IEnumerator RutinaBrillos()
     {
-        Debug.Log("Time " + Time.time);
-
-        Image tmp_brillo = (Image)
-                        Instantiate(imgBrillo);
-
-        tmp_brillo.transform.SetParent(imgBanca[0].transform);
-        //Instantiate(imgBrillo, 
-        //    new Vector2(imgBanca[0].rectTransform.position.x, 
-        //        imgBanca[0].rectTransform.position.y), 
-        //    Quaternion.identity);
-        yield return new WaitForSeconds(waitTime);
         
+        yield return new WaitForSeconds(waitTime);
+        Image brillo1 = CreateShine(imgBanca[0].transform);
+        Image brillo2 = CreateShine(imgBanca[1].transform);
+        yield return new WaitForSeconds(waitTime);
+        Destroy(brillo1);
+        Destroy(brillo2);
+        yield return new WaitForSeconds(waitTime);
+        Image brillo3 = CreateShine(imgArbol[0].transform);
+        Image brillo4 = CreateShine(imgArbol[1].transform);
+        yield return new WaitForSeconds(waitTime);
+        Destroy(brillo3);
+        Destroy(brillo4);
+        yield return new WaitForSeconds(waitTime);
+        Image brillo5 = CreateShine(imgVereda.transform);
+        yield return new WaitForSeconds(waitTime);
+        Destroy(brillo5);
+        yield return new WaitForSeconds(waitTime);
+        Image brillo6 = CreateShine(imgFuente.transform);
+        yield return new WaitForSeconds(waitTime);
+        Destroy(brillo6);
+        yield return new WaitForSeconds(waitTime);
+
+        btnNext.SetActive(true);
+
+    }
+
+    private Image CreateShine(Transform t) {
+        Image tmp_image = (Image)Instantiate(imgBrillo);
+        tmp_image.transform.SetParent(t);
+        tmp_image.transform.position = new Vector2(t.position.x, t.position.y);
+        tmp_image.transform.localScale = Vector3.one;
+        return tmp_image;
+    }
+
+    private void DeactivateNextButton()
+    {
+        Transform panelMenu = this.transform.FindChild("BottomMenu");
+        btnNext = panelMenu.transform.FindChild("Next").gameObject;
+        btnNext.SetActive(false);
     }
 }
---
+++ Assets/RDC7/Scripts/P3Controller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P3Controller : MonoBehaviour {
+
+    public string[] answer;
+    //Paneles de ejercicios
+    public RectTransform[] exercises;
+
+
+    private string currentChoice;
+    private int currentExcercise = 0;
+    private GameObject btnNext;
+    private int lastIndexExcercise;
+
+    // Use this for initialization
+    void Start () {
+        DeactivateNextButton();
+        exercises[currentExcercise].gameObject.SetActive(true);
+        lastIndexExcercise = exercises.Length - 1;
+	}
+	
+
+    private void DeactivateNextButton()
+    {
+        Transform panelMenu = this.transform.FindChild("BottomMenu");
+        btnNext = panelMenu.transform.FindChild("Next").gameObject;
+        btnNext.SetActive(false);
+    }
+
+    private void CheckGameState() {
+        if (currentChoice == answer[currentExcercise] && currentExcercise < lastIndexExcercise)
+        {
+            Debug.Log("Right Choice Sgte ejercicio");
+            exercises[currentExcercise].gameObject.SetActive(false);
+            currentExcercise++;
+            exercises[currentExcercise].gameObject.SetActive(true);
+        }
+        else if (currentChoice == answer[currentExcercise] && currentExcercise == lastIndexExcercise) {
+            btnNext.SetActive(true);
+            Debug.Log("Right Choice Fin de los ejercicios");
+        }
+        else {
+            Debug.Log(currentChoice);
+        }
+    }
+
+    public void OpcionA() {
+        currentChoice = "A";
+        CheckGameState();
+    }
+
+    public void OpcionB()
+    {
+        currentChoice = "B";
+        CheckGameState();
+    }
+
+    public void OpcionC()
+    {
+        currentChoice = "C";
+        CheckGameState();
+    }
+}
