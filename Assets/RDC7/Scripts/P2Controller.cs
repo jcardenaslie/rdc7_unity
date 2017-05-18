0982d66755f67ad9f75cf1bf29fcd83c66b08f0b
Joaquin Cardenas <jcardenas.lie@gmail.com>
2017-05-17 20:30:06 -0400

    Inicio Pantalla2

---
+++ Assets/RDC7/Scripts/P2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class P2Controller : MonoBehaviour {
+
+    Image []images;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
